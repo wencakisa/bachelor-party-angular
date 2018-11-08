54dbdf1c058f979f40a06774e08409378b155bdf
wencakisa <wencakisa@gmail.com>
2018-11-08 11:48:16 +0200

    Activity detail component

---
+++ src/app/activity-detail/activity-detail.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
+import { Activity } from '../activity';
+import { ActivityService } from '../activity.service';
+
+@Component({
+  selector: 'app-activity-detail',
+  templateUrl: './activity-detail.component.html',
+  styleUrls: ['./activity-detail.component.css']
+})
+export class ActivityDetailComponent implements OnInit {
+
+  @Input() activity: Activity;
+
+  constructor(
+    private route: ActivatedRoute,
+    private activityService: ActivityService,
+    private location: Location
+  ) { }
+
+  ngOnInit() {
+    this.getActivity();
+  }
+
+  getActivity(): void {
+    const id = +this.route.snapshot.paramMap.get('id');
+
+    this.activityService.getActivity(id)
+      .subscribe(activity => this.activity = activity);
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
+}
