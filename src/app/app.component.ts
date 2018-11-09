027163c839b230c4cc4edcf1f7722359b39989a3
wencakisa <wencakisa@gmail.com>
2018-11-10 00:24:56 +0200

    Changed app title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'bachelor-party-js';
+  title = 'Bachelor Party Sofia';
 }
