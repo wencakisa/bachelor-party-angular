5bcf095d374867c9a5853401e816604eb95e2695
wencakisa <wencakisa@gmail.com>
2018-11-10 00:25:40 +0200

    Now redirecting empty path to dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { ActivitiesComponent } from './activities/activities.component';
 import { ActivityDetailComponent } from './activity-detail/activity-detail.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'activities', pathMatch: 'full' },
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },
   { path: 'activities', component: ActivitiesComponent },
   { path: 'activities/:id', component:  ActivityDetailComponent }
 ]
