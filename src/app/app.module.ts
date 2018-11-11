de9e91a98af1543e13b1721318ecbc78b2ef31d6
wencakisa <wencakisa@gmail.com>
2018-11-11 17:47:30 +0200

    Ordered imports in app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AngularTokenModule } from 'angular-token';
 
