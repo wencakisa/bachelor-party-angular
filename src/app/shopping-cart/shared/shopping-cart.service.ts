3ae2f94c2c2c5ac5c0fb17c789f1679b50357c5c
wencakisa <wencakisa@gmail.com>
2018-11-13 11:21:17 +0200

    Activities in shopping cart art are now stored in local storage

--- src/app/shopping-cart/shared/shopping-cart.service.ts
+++ src/app/shopping-cart/shared/shopping-cart.service.ts
@@ -3,12 +3,14 @@ import { Injectable } from '@angular/core';
 import { Activity } from '../../activities/shared/activity.model';
 import { BehaviorSubject, Observable } from 'rxjs';
 
+const ACTIVITIES_IN_CART_LS_KEY = 'activitiesInCart';
+
 @Injectable({
   providedIn: 'root'
 })
 export class ShoppingCartService {
 
-  private activitiesInCartSubject: BehaviorSubject<Activity[]> = new BehaviorSubject([]);
+  private activitiesInCartSubject: BehaviorSubject<Activity[]> = new BehaviorSubject(this.getActivitiesFromLocalStorage());
   private activitiesInCart: Activity[] = [];
 
   constructor() {
@@ -17,15 +19,30 @@ export class ShoppingCartService {
   }
 
   addToCart(activity: Activity): void {
-    if (!this.activitiesInCart
-          .find(activityInCart => activityInCart === activity)) {
-      this.activitiesInCartSubject.next([...this.activitiesInCart, activity]);
-    }
+    if (this.canAddActivity(activity)) {
+      this.activitiesInCart.push(activity);
+      this.activitiesInCartSubject.next(this.activitiesInCart);
 
-    console.log('You can not an activity more than once!');
+      this.updateLocalStorageActivities();
+    } else {
+      console.log(`Activity '${activity.title}' is already in your cart!`);
+    }
   }
 
   getActivities(): Observable<Activity[]> {
     return this.activitiesInCartSubject;
+  }
+
+  private canAddActivity(activity: Activity) {
+    return !this.activitiesInCart
+      .find(activityInCart => activityInCart.id === activity.id)
+  }
+
+  private getActivitiesFromLocalStorage(): Activity[] {
+    return JSON.parse(localStorage.getItem(ACTIVITIES_IN_CART_LS_KEY)) || [];
+  }
+
+  private updateLocalStorageActivities(): void {
+    localStorage.setItem(ACTIVITIES_IN_CART_LS_KEY, JSON.stringify(this.activitiesInCart));
   }
 }
