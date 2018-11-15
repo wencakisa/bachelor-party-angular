e657f971b79717fcf9ec50e42b977888efcf8917
wencakisa <wencakisa@gmail.com>
2018-11-15 12:29:56 +0200

    Exposed localStorage key in global config

--- src/app/app.settings.ts
+++ src/app/app.settings.ts
@@ -1,3 +1,6 @@
 export class AppSettings {
-  public static API_BASE = 'http://localhost:3000'
+  public static API_BASE = 'http://localhost:3000';
+
+  public static ACTIVITIES_IN_CART_LS_KEY = 'activitiesInCart';
+  public static GROUP_SIZE_LS_KEY = 'groupSize';
 }
