448605201139f852817fc8e8c9373941d4ab7e31
inc3ndium <k.madjunov@gmail.com>
2018-11-26 02:22:04 +0200

    Add edit activity component and delete activity functionality

--- src/app/activities/activity-list/activity-list.component.ts
+++ src/app/activities/activity-list/activity-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import { Activity } from '../shared/activity.model';
 import { ActivityService } from '../shared/activity.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-activity-list',
@@ -12,7 +13,7 @@ export class ActivityListComponent implements OnInit {
 
   activities: Activity[];
 
-  constructor(private activityService: ActivityService) { }
+  constructor(private activityService: ActivityService, private router: Router) { }
 
   ngOnInit() {
     this.getActivities();
@@ -21,5 +22,16 @@ export class ActivityListComponent implements OnInit {
   getActivities(): void {
     this.activityService.getActivities()
       .subscribe(activities => this.activities = activities);
+  }
+
+  editActivity(activity: Activity) {
+    this.router.navigate([`/activities/${activity.id}/edit`]);
+  }
+
+  deleteActivity(activity: Activity) {
+    if (confirm("Are you sure you want to delete this activity?")) {
+      this.activityService.deleteActivity(activity.id)
+        .subscribe(activities => this.activities = this.activities.filter(a => a !== activity));
+    }
   }
 }
