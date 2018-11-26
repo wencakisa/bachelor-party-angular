dc9cde5d2d1afba963cf06b66b0a14ae3b8c57d6
inc3ndium <k.madjunov@gmail.com>
2018-11-26 02:27:45 +0200

    Add activity create, edit and delete routes, methods and components respectively to routing module, service and module

--- src/app/activities/activities-routing.module.ts
+++ src/app/activities/activities-routing.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { ActivityListComponent } from './activity-list/activity-list.component';
+import { ActivityCreateComponent } from './activity-create/activity-create.component';
 import { ActivityDetailComponent } from './activity-detail/activity-detail.component';
+import { ActivityEditComponent } from './activity-edit/activity-edit.component';
+import { AdminAuthGuard } from '../admin/guards/admin-auth.guard';
 
 const activityRoutes: Routes = [
   { path: 'activities', component: ActivityListComponent },
-  { path: 'activities/:id', component:  ActivityDetailComponent }
+  { path: 'activity/new', component:  ActivityCreateComponent, canActivate: [AdminAuthGuard] },
+  { path: 'activities/:id', component:  ActivityDetailComponent },
+  { path: 'activities/:id/edit', component:  ActivityEditComponent, canActivate: [AdminAuthGuard] }
 ]
 
 @NgModule({
