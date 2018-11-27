56288792bb29c8aaed516159e7c050b606b07188
wencakisa <wencakisa@gmail.com>
2018-11-27 22:49:47 +0200

    Added more detailed information about pricing in activity list view

--- src/app/activities/shared/activity.model.ts
+++ src/app/activities/shared/activity.model.ts
@@ -1,5 +1,6 @@
 import { Price } from './price.model';
 import { Deserializable } from 'src/app/shared/deserializable.model';
+import { AppSettings } from 'src/app/app.settings';
 
 export class Activity implements Deserializable {
   id: number;
@@ -20,5 +21,9 @@ export class Activity implements Deserializable {
 
   getLowestPriceAmount(): number {
     return this.prices.map(price => price.amount).reduce((a, b) => Math.min(a, b))
+  }
+
+  getDefaultCurrency(): string {
+    return AppSettings.DEFAULT_CURRENCY;
   }
 }
