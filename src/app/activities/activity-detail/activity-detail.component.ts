0a17d639bb5b37558f0712cfe4a0fd9274680c7d
wencakisa <wencakisa@gmail.com>
2018-12-07 23:49:31 +0200

    Injected shoppingCartService in activity detail component used for checking whether an activity is presented in shopping cart

--- src/app/activities/activity-detail/activity-detail.component.ts
+++ src/app/activities/activity-detail/activity-detail.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
-import { Activity } from '../shared/activity.model';
 import { ActivityService } from '../shared/activity.service';
+import { Activity } from '../shared/activity.model';
+import { ActivityInCart } from '../shared/activityInCart.model';
 import { Price } from '../shared/price.model';
+
+import { ShoppingCartService } from '../../shopping-cart/shared/shopping-cart.service';
 
 @Component({
   selector: 'app-activity-detail',
@@ -14,10 +17,12 @@ export class ActivityDetailComponent implements OnInit {
 
   @Input() activity: Activity;
   selectedPrice: Price;
+  activityFromCart: ActivityInCart;
 
   constructor(
     private route: ActivatedRoute,
-    private activityService: ActivityService
+    private activityService: ActivityService,
+    private shoppingCartService: ShoppingCartService
   ) { }
 
   ngOnInit() {
@@ -30,14 +35,23 @@ export class ActivityDetailComponent implements OnInit {
     this.activityService.getActivity(id)
       .subscribe(activity => {
         this.activity = activity;
-
+      }).add(() => {
         if (this.activity.prices) {
+          // The first price is selected by default
           this.onSelectionChange(this.activity.prices[0])
+        }
+
+        if (this.activityIsInCart()) {
+          this.activityFromCart = this.shoppingCartService.getActivityById(this.activity.id);
         }
       });
   }
 
   onSelectionChange(price: Price): void {
     this.selectedPrice = Object.assign({}, this.selectedPrice, price);
+  }
+
+  activityIsInCart(): boolean {
+    return this.shoppingCartService.activityIsInCart(this.activity);
   }
 }
