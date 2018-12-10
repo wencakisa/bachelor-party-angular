9be4ab14e867f687ccbed777c7c93a719232f39e
inc3ndium <k.madjunov@gmail.com>
2018-12-10 12:24:04 +0200

    Allow quotations listing page only for admin

--- src/app/quotations/quotations-routing.module.ts
+++ src/app/quotations/quotations-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { AdminAuthGuard } from '../admin/guards/admin-auth.guard';
 import { QuotationListComponent } from './quotation-list/quotation-list.component';
 
 const quotationRoutes: Routes = [
-  { path: 'quotations', component: QuotationListComponent }
+  { path: 'quotations', component: QuotationListComponent, canActivate: [AdminAuthGuard] }
 ]
 
 @NgModule({
