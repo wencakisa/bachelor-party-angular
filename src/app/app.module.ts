e1f83bbda31a92b8a3d87e6c9c0fd7f81dd541a2
wencakisa <wencakisa@gmail.com>
2018-12-12 15:38:07 +0200

    Added BrowserAnimationModule in app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -23,6 +24,7 @@ import { IfInRoleModule } from './shared/if-in-role.module';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     ActivitiesModule,
     AuthenticationModule,
     AdminModule,
