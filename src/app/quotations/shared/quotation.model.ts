dd7c7e4978aefd2540915530dff241e7aa2cdc1b
inc3ndium <k.madjunov@gmail.com>
2018-12-13 22:37:39 +0200

    Add id property to quotation model

--- src/app/quotations/shared/quotation.model.ts
+++ src/app/quotations/shared/quotation.model.ts
@@ -3,6 +3,7 @@ import { Activity } from 'src/app/activities/shared/activity.model';
 import { Price } from 'src/app/activities/shared/price.model';
 
 export class Quotation implements Deserializable {
+  id: number;
   group_size: number;
   user_email: string;
   activities: Activity[];
