f55f84068afff29dd3b1764f4e0258046f0cd0d5
wencakisa <wencakisa@gmail.com>
2019-02-24 12:58:57 +0200

    Additional properties for Party model

--- src/app/parties/shared/party.model.ts
+++ src/app/parties/shared/party.model.ts
@@ -1,11 +1,15 @@
 import { Deserializable } from '../../shared/deserializable.model';
 import { Activity } from '../../activities/shared/activity.model';
+import { User } from '../../users/shared/user.model';
 
 export class Party implements Deserializable {
   id: number;
   title: string;
-  // datetime: string;
+  date: string;
   activities: Activity[];
+  host: User;
+  guide: User;
+  customers: User[];
 
   deserialize(params: any): Party {
     Object.assign(this, params)
