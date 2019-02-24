03ee49a2992a2e9326b58785953b0ad24698ad29
wencakisa <wencakisa@gmail.com>
2019-02-24 20:55:47 +0200

    Including invitation form in party detail view

--- src/app/parties/parties.module.ts
+++ src/app/parties/parties.module.ts
@@ -5,13 +5,19 @@ import { IfInRoleModule } from '../shared/if-in-role.module';
 import { PartyListComponent } from './party-list/party-list.component';
 import { PartyRoutingModule } from './party-routing.module';
 import { PartyDetailComponent } from './party-detail/party-detail.component';
+import { InvitesModule } from '../invites/invites.module';
+import { InviteFormComponent } from '../invites/shared/invite-form/invite-form.component';
 
 @NgModule({
   declarations: [PartyListComponent, PartyDetailComponent],
   imports: [
     CommonModule,
     PartyRoutingModule,
+    InvitesModule,
     IfInRoleModule,
+  ],
+  entryComponents: [
+    InviteFormComponent
   ]
 })
 export class PartiesModule { }
