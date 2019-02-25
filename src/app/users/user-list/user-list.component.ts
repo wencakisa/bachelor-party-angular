d6bcace8e81bb471a6ffcff3a5af175a6e144767
kall0m <k.madjunov@gmail.com>
2019-02-26 00:14:17 +0200

    Divide users listing page into three sections - for each type of user

--- src/app/users/user-list/user-list.component.ts
+++ src/app/users/user-list/user-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from '../shared/user.model';
 import { UserService } from '../shared/user.service';
+import { AppSettings } from 'src/app/app.settings';
 
 @Component({
   selector: 'app-user-list',
@@ -10,6 +11,9 @@ import { UserService } from '../shared/user.service';
 export class UserListComponent implements OnInit {
 
   users: User[];
+  admins: User[];
+  guides: User[];
+  customers: User[];
  
   constructor(private userService: UserService) { }
  
@@ -19,7 +23,12 @@ export class UserListComponent implements OnInit {
  
   getUsers(): void {
     this.userService.getAllUsers()
-      .subscribe(users => this.users = users);
+      .subscribe(users => {
+        this.admins = users.filter(user => user.role === AppSettings.ROLE_ADMIN);
+        this.guides = users.filter(user => user.role === AppSettings.ROLE_GUIDE);
+        this.customers = users.filter(user => user.role === AppSettings.ROLE_CUSTOMER);
+        this.users = users;
+      });
   }
 
   deleteUser(user: User) {
@@ -27,6 +36,10 @@ export class UserListComponent implements OnInit {
       this.userService.deleteUser(user.id)
         .subscribe((users: User[]) => {
           this.users = this.users.filter(u => u !== user);
+          
+          this.admins = this.users.filter(user => user.role === AppSettings.ROLE_ADMIN);
+          this.guides = this.users.filter(user => user.role === AppSettings.ROLE_GUIDE);
+          this.customers = this.users.filter(user => user.role === AppSettings.ROLE_CUSTOMER);
         });
     }
   }
